6559f46d34ce6e7f31fe7955e0a2129b3b876fe1
Bahaasrour <dev.bahaasrour@gmail.com>
2021-08-23 20:01:54 +0200

    adding movement
    Adding player movement
    Adding NavMesh agent
    Adding dummy scene for testing

---
+++ mangamTasks/Assets/_Project Assets/Scripts/FixedJoystickButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FixedJoystickButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    protected bool pressed = false;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        pressed = false;
+    }
+}
---
+++ mangamTasks/Assets/_Project Assets/Scripts/PlayerMovements.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    public Joystick Joystick;
+    public FixedJoystickButton FixedJoystickButton;
+    Rigidbody rb;
+    public float speed = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        // Joystick = FindObjectOfType<Joystick>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector3(Joystick.Horizontal * speed, rb.velocity.y, Joystick.Vertical * speed);
+    }
+}
