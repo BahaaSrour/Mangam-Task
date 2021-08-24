b3b5956671bf69c32381c1198c2610da314f090a
Bahaasrour <dev.bahaasrour@gmail.com>
2021-08-24 17:35:52 +0200

    the player with bomb die when the bomb Explode

--- mangamTasks/Assets/_Project Assets/Scripts/AllPlayers.cs
+++ mangamTasks/Assets/_Project Assets/Scripts/AllPlayers.cs
@@ -6,25 +6,42 @@ public class AllPlayers : MonoBehaviour
 {
     public ScriptableEvent newBomberMan;
     public static List<GameObject> players;
-
     public ScriptableEventWithintSO newBomberManChanged;
+    public float bombTimer;
+    public GameObject Blocker;
+
+
+    float timer;
+    [HideInInspector]
+    public float currentBombTimer;
     private void Awake()
     {
         players = new List<GameObject>();
         newBomberMan.action += PickNewPlayerToCarryTheBomb;
-       // newBomberManChanged += AttachBombToPlayer(0);
+        // newBomberManChanged += AttachBombToPlayer(0);
     }
-
     private void Start()
     {
         newBomberMan.action.Invoke();
+        currentBombTimer = bombTimer+Time.time;
+
     }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.H))
             newBomberMan.action.Invoke();
-    }
+        timer = currentBombTimer - Time.time;
+        //Debug.Log("Current Bomb " + currentBombTimer);
+        //Debug.Log("Timer " + timer);
 
+        if (timer <= 0)
+        { 
+            KillThePlayer();
+            newBomberMan.action.Invoke();
+            currentBombTimer = bombTimer + Time.time;
+        }
+
+    }
     public void PickNewPlayerToCarryTheBomb()
     {
         int x = Random.Range(0, players.Count);
@@ -40,8 +57,6 @@ public class AllPlayers : MonoBehaviour
         players[x].GetComponent<PlayerHasBomb>().HasBomb = true;
         players[x].GetComponent<PlayerHasBomb>().BombPrefab.SetActive( true);
     }
-
-
     public static void AttachBombToPlayer(int val)
     {
         for (int i = 0; i < players.Count; i++)
@@ -55,10 +70,36 @@ public class AllPlayers : MonoBehaviour
         players[val].GetComponent<PlayerHasBomb>().HasBomb = true;
         players[val].GetComponent<PlayerHasBomb>().BombPrefab.SetActive(true);
     }
-
     private void OnDisable()
     {
         newBomberMan.action -= PickNewPlayerToCarryTheBomb;
     }
 
+    public void KillThePlayer()
+    {
+        Debug.Log("nember of Players before removeing " + players.Count);
+        for (int i = 0; i < players.Count; i++)
+        {
+            if (players[i].activeSelf)
+            {
+                if (players[i].GetComponent<PlayerHasBomb>().HasBomb == true)
+                {
+                    //players.Remove(players[i]);
+                    players[i].SetActive(false);
+                    if (players[i].tag == "MainPlayer") Blocker.SetActive(true);
+                    Debug.Log("the removed player " + players[i].name);
+                };
+            }
+        }
+
+        Debug.Log("##neumber of Players after removeing" + players.Count);
+
+        for (int i = 0; i < players.Count; i++)
+        {
+            Debug.Log(players[i].name);
+        }
+
+
+
+    }
 }
