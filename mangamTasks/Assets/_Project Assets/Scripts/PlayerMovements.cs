e9d9d875053ed207208ed4b033db7358cdc581a4
Bahaasrour <dev.bahaasrour@gmail.com>
2021-08-25 09:45:25 +0200

    Editing Environment

--- mangamTasks/Assets/_Project Assets/Scripts/PlayerMovements.cs
+++ mangamTasks/Assets/_Project Assets/Scripts/PlayerMovements.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof( AudioSource))]
 public class PlayerMovements : MonoBehaviour
 {
     public Joystick Joystick;
     public FixedJoystickButton FixedJoystickButton;
     Rigidbody rb;
     public float speed = 100;
+
+    public AudioClip movingStepsSound;
+    public AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         // Joystick = FindObjectOfType<Joystick>();
+        //audioSource.GetComponent<AudioSource>();
 
     }
 
@@ -20,5 +25,9 @@ public class PlayerMovements : MonoBehaviour
     void Update()
     {
         rb.velocity = new Vector3(Joystick.Horizontal * speed, rb.velocity.y, Joystick.Vertical * speed);
+        if ((Mathf.Abs(rb.velocity.x )> 3 || Mathf.Abs(rb.velocity.z) > 3)  && audioSource.isPlaying == false)
+            audioSource.PlayOneShot(movingStepsSound);
+
+
     }
 }
